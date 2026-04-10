e6eb1f89c68e85b4885e40c82b3ca5b0569e582f
Noman <an8@digitaldolphins.jp>
2026-04-10 09:21:11 +0900

    first commit

---
+++ CsvPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsvPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
